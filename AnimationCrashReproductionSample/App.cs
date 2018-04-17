4d06fa1082dda4b0e2c8f177b3cfa5638967b1de
Brandon Minnick <brminnick@users.noreply.github.com>
2018-04-16 17:16:20 -0700

    First Commit

---
+++ AnimationCrashReproductionSample/App.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace AnimationCrashReproductionSample
+{
+	public class App : Application
+	{
+		public App() => MainPage = new NavigationPage(new SpinButtonPage());
+	}
+
+	class SpinButtonPage : ContentPage
+	{
+		public SpinButtonPage() => Content = new SpinButton { Text = "Quickly Double Tap This Button" };
+	}
+}
---
+++ AnimationCrashReproductionSample/SpinButton.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace AnimationCrashReproductionSample
+{
+	public class SpinButton : Button
+	{
+		public SpinButton() => Clicked += HandleClicked;
+
+		async void HandleClicked(object sender, System.EventArgs e)
+		{
+			await this.RotateTo(359, 500, Easing.CubicOut);
+			await this.RotateTo(0, 500, Easing.CubicOut);
+		}
+	}
+}
